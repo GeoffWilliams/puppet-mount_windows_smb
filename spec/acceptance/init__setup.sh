1f66bfb6930a5f9f17144751cd4c46ecc51f1e15
Geoff Williams <geoff@declarativesystems.com>
2019-09-18 09:33:01 +1000

    remove broken tests

--- spec/acceptance/init__setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# If this file exists it will be run on the system under test before puppet runs
-# to setup any prequisite test conditions, etc
--- spec/classes/init_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe 'mount_windows_smb' do
-  context 'with default values for all parameters' do
-    it { should contain_class('mount_windows_smb') }
-  end
-end
